97fa56f380a6070de00da38efbc4757d0dab6f86
Bohredd <dapereira@inf.ufsm.br>
2024-11-20 11:26:57 -0300

    feat: question show + example func verify answer

--- frontend/src/components/interviews/QuestionsShow.tsx
+++ frontend/src/components/interviews/QuestionsShow.tsx
@@ -1,6 +1,8 @@
 import { Button } from "react-bootstrap";
 import { QuestionShow } from "./QuestionShow";
 import { useState } from "react";
+import ListenPersonAnswer from "../../functions/ListenPerson";
+import { VerifyAnswers } from "../../functions/VerifyAnswers";
 
 interface Props {
     questions: number[];
@@ -10,12 +12,9 @@ export const QuestionsShow = ({ questions }: Props) => {
 
 
     const [actualQuestion, setActualQuestion] = useState<number>(0);
-
-    console.log("QuestionsShow: ", questions);
-
-    console.log("Actual question: ", actualQuestion);
-
-    console.log(questions);
+    const [isAnswering, setIsAnswering] = useState<boolean>(false);
+    const [answer, setAnswer] = useState<string | null>(null);
+    const [recognition, setRecognition] = useState<any>(null);
 
     const handleNextQuestion = () => {
         if (actualQuestion < questions.length - 1) {
@@ -30,11 +29,45 @@ export const QuestionsShow = ({ questions }: Props) => {
     };
 
     const handleAnswerQuestion = () => {
-        console.log("will listen");
+        try {
+            setIsAnswering(true);
+            const recognition = ListenPersonAnswer("en-US");
+            setRecognition(recognition);
+            recognition.onresult = (event: any) => {
+            const current = event.resultIndex;
+            const transcript = event.results[current][0].transcript;
+            setAnswer(transcript);
+            };
+
+            recognition.start();
+        } catch (error) {
+            console.log("error: ", error);
+        }
+    };
+
+    const handleStopAnswering = () => {
+        if (recognition) {
+            recognition.stop();
+        }
+
+        if (!answer) {
+            return;
+        }
+
+        //  todo: validate the answer 
+
+        // const isCorrect = VerifyAnswers(answer, );
+
+        setIsAnswering(false);
     };
 
     return (
       <div>
+        <h3>You are answering question {actualQuestion + 1}</h3>
+        {answer && (
+        <p>You said: {answer}</p> 
+        )}
+
         <QuestionShow questionId={questions[actualQuestion]} />
 
         <Button onClick={handlePreviousQuestion}>
@@ -42,6 +75,8 @@ export const QuestionsShow = ({ questions }: Props) => {
         </Button> 
 
         <Button onClick={handleAnswerQuestion}>Answer</Button>
+
+        <Button onClick={handleStopAnswering} disabled={!isAnswering}>Stop</Button>
 
         <Button onClick={handleNextQuestion}>
           Next
