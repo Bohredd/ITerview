6bbd71afacdc7485bb855ff1ac1c264c5690951f
Bohredd <dapereira@inf.ufsm.br>
2024-11-21 15:33:18 -0300

    feat: user + plans + payment app

--- backend/plans/models.py
+++ backend/plans/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Plans(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    price = models.IntegerField()
+    max_daily_participations = models.IntegerField()
+    max_interview_participations = models.IntegerField()
+    max_common_sentences = models.IntegerField()
+
+    class Meta:
+        db_table = "plans"
+
+    def __str__(self):
+        return self.title + " - U$" + str(self.price) 
+    
+class UserPlan(models.Model):
+    plan = models.ForeignKey(Plans, on_delete=models.CASCADE)
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    transactions = models.ManyToManyField("payment.Transaction", blank=True)
