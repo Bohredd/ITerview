836739fe17aa5be140b67e78b06f49b404fe2d20
Bohredd <dapereira@inf.ufsm.br>
2024-11-23 20:57:43 -0300

    feat: start implement can play this game

--- frontend/src/views/interviews/Interview.tsx
+++ frontend/src/views/interviews/Interview.tsx
@@ -3,16 +3,51 @@ import { useState } from "react"
 import useFetchData from "../../functions/FetchData"
 import { useParams } from "react-router-dom"
 import { QuestionsShow } from "../../components/interviews/QuestionsShow"
+import axios from "axios"
 
 export const InterviewView = () => {
 
     const { id } = useParams<{ id: string }>();
 
-    // console.log(id);
-
     const [interview, setInterview] = useState<Interview | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+
+    const userToken = localStorage.getItem("authToken");
+
+    console.log("userToken", userToken);
+
+    // TODO: continuar a implementacao de can play this game 
+
+    const canPlay = async () => {
+
+        const body = {
+            "user_token": userToken,
+            "game_name": "Fake Job Interview",
+        }
+
+        try {
+            const response = await axios.post(
+                "http://127.0.0.1:8000/payments/api/can_play_game/",
+                body,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            return response.data.can_play;
+        } catch (error) {
+            console.error("Error fetching data", error);
+            return false;
+        }
+    };
+
+    const canPlayInterview = canPlay();
+
+    if (!canPlayInterview) {
+        return <div>Cannot play this interview</div>;
+    }
 
     useFetchData<Interview>({
         method: "GET",
