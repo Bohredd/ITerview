2a16e5259bec9c116677020243892e0bff79d5f6
Bohredd <dapereira@inf.ufsm.br>
2024-11-29 17:54:53 -0300

    feat: signals + task payment verif

--- backend/jobs/tasks/payment/payment_verification.py
+++ backend/jobs/tasks/payment/payment_verification.py
@@ -1,24 +1,73 @@
 from celery import shared_task
 from celery.exceptions import MaxRetriesExceededError
-
-
+from payment.models import Transaction
+import mercadopago  
+from decouple import config
+import datetime
+from plans.models import Plans, UserPlan
 @shared_task(
     bind=True, max_retries=24, default_retry_delay=300, name="check_payment_status_task"
 ) # default retry delay is 5 minutes
-def check_payment_status_task(self, payment_id):
-    try:
-        payment_successful = False
+def check_payment_status_task(self, hash_id):
 
-        print("Pagamento com sucesso? ", payment_successful)
+    transaction = Transaction.objects.get(hash_id=hash_id)
+    
+    payment_successful = False
+
+    if transaction.status == "approved":
+        payment_successful = True
+
+    try:
+        user = transaction.user_set.first()
+
+        if config("DEBUG", default=False, cast=bool):
+            sdk = mercadopago.SDK(config("MERCADO_PAGO_ACCESS_TOKEN_TEST"))
+        else:
+            sdk = mercadopago.SDK(config("MERCADO_PAGO_ACCESS_TOKEN_PRD"))
+
+        payment = sdk.payment().search({"external_reference": hash_id})
+
+        if payment["response"]["results"][0]["status"] == "approved":
+            transaction.status = "approved"
+
+            payment_method = payment["response"]["results"][0]["payment_method_id"]
+
+            if payment_method == "credit_card":
+                transaction.payment_method = "credit_card"
+            else:
+                transaction.payment_method = "pix"
+
+            transaction.updated_at = datetime.datetime.now()
+            transaction.save()
+
+            user_plan = UserPlan.objects.filter(user=user)
+
+            if not user_plan.exists():
+                UserPlan.objects.create(user=user, plan=Plans.objects.get(title="Junior"))
+
+                user_plan = UserPlan.objects.get(user=user)
+
+            else:
+                user_plan = user_plan.first()
+
+            user_plan.plan = transaction.plan_acquired
+            user_plan.active = True
+            user_plan.changed_at = datetime.datetime.now()
+            user_plan.end_date = datetime.datetime.now() + datetime.timedelta(days=30)
+            user_plan.save()
+
+            payment_successful = True
+        else:
+            payment_successful = False
 
         if payment_successful:
-            print(f"Payment {payment_id} has been successful!")
+            print(f"Payment {hash_id} has been successful!")
             return "Payment successful" 
 
         else:
-            print(f"Payment {payment_id} not successful. Retrying...")
+            print(f"Payment {hash_id} not successful. Retrying...")
             raise self.retry() 
 
     except MaxRetriesExceededError:
-        print(f"Maximum retry attempts reached for payment {payment_id}.")
+        print(f"Maximum retry attempts reached for payment {hash_id}.")
         return "Payment verification failed after maximum retries"
--- backend/payment/signals.py
+++ backend/payment/signals.py
@@ -2,9 +2,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from jobs.tasks.payment.payment_verification import check_payment_status_task
 from .models import Transaction
+from plans.models import Plans
 
 @receiver(post_save, sender=Transaction)
 def transaction_created(sender, instance, created, **kwargs):
-    if created:
-        print("Transaction created")
-        check_payment_status_task.apply_async(args=[instance.id])
+    if created and instance.plan_acquired != Plans.objects.get(title="Junior") and instance.status != "approved":
+        check_payment_status_task.apply_async(args=[instance.hash_id])
