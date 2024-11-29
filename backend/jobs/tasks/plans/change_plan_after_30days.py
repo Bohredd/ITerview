e2c6d1230c944d08d555bef9d9595ac82374923e
Bohredd <dapereira@inf.ufsm.br>
2024-11-29 15:09:59 -0300

    feat: change plan task

--- backend/jobs/tasks/plans/change_plan_after_30days.py
+++ backend/jobs/tasks/plans/change_plan_after_30days.py
@@ -1,13 +1,22 @@
 from celery import shared_task
 from celery.exceptions import MaxRetriesExceededError
-
+from user.models import User
+from plans.models import UserPlan, Plans
+from django.utils import timezone
 
 @shared_task(name="change_plan_after_30days_task")
 def change_plan_after_30days_task(user_id):
-    try:
-        print(f"Changing plan for user {user_id} after 30 days!")
-        return "Plan changed after 30 days"
 
-    except MaxRetriesExceededError:
-        print(f"Maximum retry attempts reached for changing plan for user {user_id}.")
-        return "Plan change failed after maximum retries"
+    user = User.objects.get(pk=user_id)
+
+    user_plan = UserPlan.objects.get(user=user)
+
+    plan_junior = Plans.objects.get(title="Junior")
+
+    user_plan.plan = plan_junior
+    user_plan.active = True
+    user_plan.changed_at = timezone.now()
+    user_plan.updated_at = timezone.now()
+    user_plan.save()
+
+    return "Plan changed to Junior default again after 30 days"
--- backend/plans/signals.py
+++ backend/plans/signals.py
@@ -9,11 +9,10 @@ def plan_changed(sender, instance, **kwargs):
     if instance.pk: 
         previous_instance = UserPlan.objects.get(pk=instance.pk)
         if previous_instance.plan != instance.plan:
-            print("oi") 
 
             time_to_run = 30 * 24 * 60 * 60 # 30 days
             time_to_run_1_minute = 60 # 1 minute
 
             changer_plan.change_plan_after_30days_task.apply_async(
-                args=[instance.user.id], countdown= time_to_run
+                args=[instance.user.id], countdown=time_to_run
             )
