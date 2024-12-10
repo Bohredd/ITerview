53d13c7cc2a03ee3bef76ba1bbaa620ef22e0621
Bohredd <dapereira@inf.ufsm.br>
2024-12-10 13:34:55 -0300

    feat: settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://iterview-production.up.railway.app/",
+    "https://iterview-production.up.railway.app",
 ]
 
 INSTALLED_APPS = [
