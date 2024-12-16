1de5966aa9f780fe4f8d1f02ec6dffe83f110d09
Bohredd <dapereira@inf.ufsm.br>
2024-12-16 19:01:41 -0300

    feat: vite config

--- backend/frontend/vite.config.ts
+++ backend/frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -11,5 +10,5 @@ export default defineConfig({
     host: "0.0.0.0", 
     port: 3000,
   },
-  base : "./",  
+  base : "/",  
 });
