2febc825666503496f0a81de0e5f1fc4ea8ff5d9
Bohredd <dapereira@inf.ufsm.br>
2024-12-16 17:17:36 -0300

    feat: static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -191,6 +191,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/dist/static"),
 ]
 
-# STATIC_ROOT = os.path.join(
-#     BASE_DIR, "staticfiles"
-# )  
+STATIC_ROOT = BASE_DIR / "static"
